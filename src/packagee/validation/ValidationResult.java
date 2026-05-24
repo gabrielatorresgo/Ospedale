d82893eda37139cdaa5e46350a8dc8129a353678
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 17:52:44 -0500

    Update ValidationResult.java

--- src/packagee/validation/ValidationResult.java
+++ src/packagee/validation/ValidationResult.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.validation;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-/**
- *
- * @author danip
- */
+
 public class ValidationResult {
     private final List<String> errors;
 
